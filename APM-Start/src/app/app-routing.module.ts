9fa6d96ed2bf140fe11f149f9cad0fc0215008a4
Adnan Salahuddin <adnan.salah84@gmail.com>
2020-04-02 22:21:42 +0500

    implemented rout guard

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             { path: 'welcome', component: WelcomeComponent },
+            {
+                path: 'products',
+                //canLoad: [AuthGuard],
+                canActivate: [AuthGuard],
+                data: { preload: true },
+                loadChildren: () => import('./products/product.module').then(m => m.ProductModule)
+            },
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: '**', component: PageNotFoundComponent },
-        ], { enableTracing: true }),
+        ], { preloadingStrategy: SelectiveStrategy }) //{ enableTracing: true } // { preloadingStrategy: PreloadAllModules }
     ],
     exports: [RouterModule]
 })
